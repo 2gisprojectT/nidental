158d02575ef13ef923cf0ae963c80b00e99c309a
Nidental Egor <nidentalegor@gmail.com>
2015-04-28 23:27:15 +0600

    Перенес теперь единственное ожидание в SetUp

--- SeleniumTest/2GisWikiTest.py
+++ SeleniumTest/2GisWikiTest.py
@@ -8,7 +8,7 @@ class SeleniumTest(TestCase):
     def setUp(self):
         self.driver = webdriver.Firefox()
         self.driver.get("https://google.ru")
-
+        self.driver.implicitly_wait(5)
         # self.driver = webdriver.Remote("http://localhost:9515",webdriver.DesiredCapabilities.CHROME)
         # self.driver.get("https://google.ru")
 
@@ -19,18 +19,15 @@ class SeleniumTest(TestCase):
         search = self.driver.find_element_by_name("q")
         search.send_keys("2гис dbrb")
         search.submit()
-        self.driver.implicitly_wait(5)
+
         link = self.driver.find_element_by_tag_name('i')
         link.click()
-        self.driver.implicitly_wait(5)
         continue_link = self.driver.find_element_by_tag_name('h3').find_element_by_tag_name('a')
-        self.driver.implicitly_wait(5)
         continue_link.click()
         self.driver.switch_to_window(self.driver.window_handles[-1])
-        # title=self.driver.title
         head = self.driver.find_element_by_id("firstHeading")
         self.assertEqual('2ГИС', head.text)
-        self.driver.close()
+        self.driver.quit()# to close both windows
 
 
 if __name__ == '__main__':
